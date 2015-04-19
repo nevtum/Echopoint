457de4d984e74d19b5220025b83c1708f932752b
Neville Tummon <nt.devs@gmail.com>
2015-04-19 19:55:22 +1000

    added project setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='Echopoint',
+      version='0.1',
+      description = 'A lightweight pub-sub library.',
+      long_description = readme(),
+      classifiers = [
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'License :: OSI Approved :: Apache Software License',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+      ],
+      keywords = 'pub sub event sourcing es',
+      url = 'https://github.com/nevtum/Echopoint.git',
+      author = 'Neville Tummon',
+      license = 'Apache License 2.0',
+      packages = ['Echopoint']
+)
